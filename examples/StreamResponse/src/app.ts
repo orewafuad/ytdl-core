1c0db7219b7bc2f1b917929c7ae4f0f27b711852
YBD Project <95850410+ybd-project@users.noreply.github.com>
2024-09-11 22:51:57 +0900

    2024/09/11 23:00

---
+++ examples/StreamResponse/src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import { YtdlCore } from '@ybd-project/ytdl-core';
+
+const app = express(),
+    ytdl = new YtdlCore({
+        lang: 'en',
+    });
+
+app.get('/api/download', (req, res) => {
+    const VIDEO_ID = req.query.videoId;
+
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
+
+    ytdl.download(`https://www.youtube.com/watch?v=${VIDEO_ID}`, {
+        filter: 'audioandvideo',
+    }).pipe(res);
+});
+
+app.get('/api/download/audio', (req, res) => {
+    const VIDEO_ID = req.query.videoId;
+
+    res.setHeader('Content-Type', 'audio/mp3');
+    res.setHeader('Content-Disposition', 'attachment; filename="audio.mp3"');
+
+    ytdl.download(`https://www.youtube.com/watch?v=${VIDEO_ID}`, {
+        filter: 'audioonly',
+    }).pipe(res);
+});
+
+app.get('/api/download', (req, res) => {
+    const VIDEO_ID = req.query.videoId;
+
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
+
+    ytdl.download(`https://www.youtube.com/watch?v=${VIDEO_ID}`, {
+        filter: 'videoonly',
+    }).pipe(res);
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
